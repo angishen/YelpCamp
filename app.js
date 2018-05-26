de2e9c243883173e1a14f6ace096bd668b75806f
angishen <angelashen01@gmail.com>
2018-05-26 06:12:39 +0000

    add show route and page

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 // Schema setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -18,7 +19,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "Bridalveil Creek",
-//         image: "https://www.nps.gov/yose/planyourvisit/images/IMG_6796edit.jpg?maxwidth=650&autorotate=false"
+//         image: "https://www.nps.gov/yose/planyourvisit/images/IMG_6796edit.jpg?maxwidth=650&autorotate=false",
+//         description: "Cool campground in Yosemite National Park, California"
 //     }, function(err, campground){
 //         if (err){
 //             console.log(err);
@@ -32,7 +34,7 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-
+// INDEX route
 app.get("/campgrounds", function(req, res){
     // retrieve campgrounds from db
     Campground.find({}, function(err, allCampgrounds){
@@ -40,21 +42,24 @@ app.get("/campgrounds", function(req, res){
             console.log(err);
         } else {
             // render campgrounds page from ejs
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
 
+// NEW route
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
+// CREATE route
 app.post("/campgrounds", function(req, res){
     // get data from form
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
     //add new campground object to campground list
-    var newCampground = {name: name, image: image};
+    var newCampground = {name: name, image: image, description: desc};
     // create a new campground and save to db
     Campground.create(newCampground, function(err, campground){
         if (err) {
@@ -65,6 +70,19 @@ app.post("/campgrounds", function(req, res){
         }
     });
     
+});
+
+// SHOW route
+app.get("/campgrounds/:id", function(req, res){
+    // find campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err) {
+            console.log(err);
+        } else {
+            // render the show template
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
