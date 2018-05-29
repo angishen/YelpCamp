863b2b2e41f528099fba7a901190a6e98e09b880
angishen <angelashen01@gmail.com>
2018-05-29 05:32:22 +0000

    create seed file and add comments model

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-var express    = require('express'),
-    app        = express(),
-    bodyParser = require('body-parser'),
-    mongoose   = require('mongoose');
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose'),
+    Campground  = require('./models/campground'),
+    Comment     = require('./models/comment'),
+    seedDB      = require("./seeds.js");
 
+seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// Schema setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "Bridalveil Creek",
-//         image: "https://www.nps.gov/yose/planyourvisit/images/IMG_6796edit.jpg?maxwidth=650&autorotate=false",
-//         description: "Cool campground in Yosemite National Park, California"
-//     }, function(err, campground){
-//         if (err){
-//             console.log(err);
-//         } else {
-//             console.log("Newly created campground: ");
-//             console.log(campground)
-//         }
-//     });
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -75,7 +56,7 @@ app.post("/campgrounds", function(req, res){
 // SHOW route
 app.get("/campgrounds/:id", function(req, res){
     // find campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if (err) {
             console.log(err);
         } else {
